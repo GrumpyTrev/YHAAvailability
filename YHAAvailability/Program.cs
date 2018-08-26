d29b6e5b7a58a541cbb8239baf4c2792f8f3be6c
Grumpy Trev <t.simmonds.chip@gmail.com>
2018-08-26 18:32:49 +0100

    Rerun for September and October

--- YHAAvailability/Program.cs
+++ YHAAvailability/Program.cs
@@ -14,36 +14,35 @@ namespace YHAAvailability
 	{
 		static void Main( string[] args )
 		{
-//			MonthRequest[] monthRequests = { new MonthRequest( "032018", "Mar" ), new MonthRequest( "042018", "Apr" ), new MonthRequest( "052018", "May" ),
-//				new MonthRequest( "062018", "Jun" ), new MonthRequest( "072018", "Jly" ), new MonthRequest( "082018", "Aug" ), new MonthRequest( "092018", "Sep" ),
-//				new MonthRequest( "102018", "Oct" ) };
+//			MonthRequest[] monthRequests = { new MonthRequest( "2018-05-01", "May" ), new MonthRequest( "2018-06-01", "Jun" ),
+//				new MonthRequest( "2018-07-01", "Jly" ), new MonthRequest( "2018-08-01", "Aug" ), new MonthRequest( "2018-09-01", "Sep" ),
+//				new MonthRequest( "2018-10-01", "Oct" ) };
 
-			MonthRequest[] monthRequests = { new MonthRequest( "052018", "May" ), new MonthRequest( "062018", "Jun" ), new MonthRequest( "072018", "Jly" ),
-				new MonthRequest( "082018", "Aug" ), new MonthRequest( "092018", "Sep" ), new MonthRequest( "102018", "Oct" ) };
+			MonthRequest[] monthRequests = { new MonthRequest( "2018-09-01", "Sep" ), new MonthRequest( "2018-10-01", "Oct" ) };
 
-			List<HostelGroup> groups = new List<HostelGroup>();
-
-			groups.Add( new HostelGroup( "Peak District" ).AddHostel( new Hostel( "Ilam Hall", 118 ), new Hostel( "Hartington Hall", 104 ), 
+			List<HostelGroup> groups = new List<HostelGroup> {
+				new HostelGroup( "Peak District" ).AddHostel( new Hostel( "Ilam Hall", 118 ), new Hostel( "Hartington Hall", 104 ),
 				new Hostel( "Edale", 81 ), new Hostel( "Castleton", 52 ), new Hostel( "Hathersage", 106 ), new Hostel( "Eyam", 93 ), new Hostel( "Ravenstor", 195 ),
-				new Hostel( "Youlgreave", 248 ) ) );
-//			groups.Add( new HostelGroup( "Peak District" ).AddHostel( new Hostel( "Ilam Hall", 118 ), 
-//				new Hostel( "Hartington Hall", 104 ) ) );
+				new Hostel( "Youlgreave", 248 ) ),
 
-			groups.Add( new HostelGroup( "Pembrokeshire" ).AddHostel( new Hostel( "Broad Haven", 35 ), new Hostel( "St Davids", 200 ), 
-				new Hostel( "Pwll Deri", 193 ), new Hostel( "Newport", 254 ), new Hostel( "Poppit Sands", 190 ), new Hostel( "Manorbier", 167 ) ) );
+				new HostelGroup( "Pembrokeshire" ).AddHostel( new Hostel( "Broad Haven", 35 ), new Hostel( "St Davids", 200 ),
+				new Hostel( "Pwll Deri", 193 ), new Hostel( "Newport", 254 ), new Hostel( "Poppit Sands", 190 ), new Hostel( "Manorbier", 167 ) ),
 
-			groups.Add( new HostelGroup( "Lakes" ).AddHostel( new Hostel( "Skiddaw", 745 ), new Hostel( "Keswick", 129 ),
+				new HostelGroup( "Lakes" ).AddHostel( new Hostel( "Skiddaw", 745 ), new Hostel( "Keswick", 129 ),
 				new Hostel( "Buttermere", 40 ), new Hostel( "Borrowdale", 157 ), new Hostel( "Ennerdale", 88 ), new Hostel( "Black Sail", 21 ),
 				new Hostel( "Honister Hause", 115 ), new Hostel( "Helvellyn", 111 ), new Hostel( "Patterdale", 182 ), new Hostel( "Ambleside", 4 ),
 				new Hostel( "Grasmere", 98 ), new Hostel( "Langdale", 112 ), new Hostel( "Coniston", 62 ), new Hostel( "Coniston Copper", 61 ),
-				new Hostel( "Eskdale", 90 ), new Hostel( "Wasdale Hall", 234 ) ) );
+				new Hostel( "Eskdale", 90 ), new Hostel( "Wasdale Hall", 234 ) ),
 
-			groups.Add( new HostelGroup( "Yorkshire Dales" ).AddHostel( new Hostel( "Osmotherley", 757 ), new Hostel( "Helmsley", 110 ),
+				new HostelGroup( "Yorkshire Dales" ).AddHostel( new Hostel( "Osmotherley", 757 ), new Hostel( "Helmsley", 110 ),
 				new Hostel( "Whitby", 337 ), new Hostel( "Scarborough", 767 ), new Hostel( "Boggle Hole", 24 ), new Hostel( "Dalby Forest", 148 ),
-				new Hostel( "York", 247 ) ) );
+				new Hostel( "York", 247 ) )
+			};
 
 			foreach ( HostelGroup group in groups )
 			{
+				int hostelCount = 0;
+
 				foreach ( Hostel hostel in group.Hostels )
 				{
 					foreach ( MonthRequest request in monthRequests )
@@ -52,17 +51,24 @@ namespace YHAAvailability
 						HtmlDocument doc = new HtmlDocument();
 						doc.LoadHtml( MakeRequest( hostel.Id.ToString( "D6" ), request.Request ).Result );
 
-						// Strip out the calendar table data
-						List<List<string>> table = doc.DocumentNode.SelectSingleNode( "//table[@class='pafinder_calendar']" )
-							.Descendants( "tr" )
-							.Skip( 1 )
-							.Where( tr => tr.Elements( "td" ).Count() > 1 )
-							.Select( tr => tr.Elements( "td" ).Select( td => td.InnerText.Trim() ).ToList() )
-							.ToList();
+						HtmlNodeCollection dormNodes = doc.DocumentNode.SelectNodes( "//div[@class='dates dorm']" );
 
-						// Parse and save to the AvailabilityData 
-						SaveAvailabiltyForHostelMonth( table, request.Name, group.Availability );
+						if ( dormNodes.Count > 0 )
+						{
+							List<string> dates = dormNodes[0].Descendants( "span" ).Select( data => data.Attributes[ "data-date" ].Value ).ToList();
+							List<string> availabilities = null;
+
+							if ( dormNodes.Count > 1 )
+							{
+								availabilities = dormNodes[ 1 ].Descendants( "span" ).Select( data => data.ChildNodes[ "#text" ].InnerText ).ToList();
+							}
+
+							// Parse and save to the AvailabilityData 
+							SaveAvailabiltyForHostelMonth( dates, availabilities, group.Availability, hostelCount );
+						}
 					}
+
+					hostelCount++;
 				}
 			}
 
@@ -80,8 +86,9 @@ namespace YHAAvailability
 		/// <returns></returns>
 		static async Task<string> MakeRequest( string hostel, string month )
 		{
-			HttpResponseMessage response = await client.PostAsync( "https://www.yha.org.uk/availability-calendar", 
-				new FormUrlEncodedContent( new Dictionary<string, string> { { "house", hostel }, { "monthyeara", month } } ) );
+			HttpResponseMessage response = await client.PostAsync( "https://availabilitycalendar.yha.org.uk/availabilityCalendar.php", 
+				new FormUrlEncodedContent( new Dictionary<string, string> { { "house", hostel }, { "viewdate", month }, { "filter", "dorm" },
+					{ "males", "1" }, { "type", "020" } } ) );
 
 			return await response.Content.ReadAsStringAsync();
 		}
@@ -92,39 +99,35 @@ namespace YHAAvailability
 		/// <param name="responseData"></param>
 		/// <param name="monthName"></param>
 		/// <param name="availability"></param>
-		static void SaveAvailabiltyForHostelMonth( List<List<string>> responseData, string monthName, AvailabilityData availability )
+		static void SaveAvailabiltyForHostelMonth( List<string> dates, List<string> availabilities, AvailabilityData availability, int hostelCount )
 		{
-			// Only process the requested month - stop when the current day is less than the previous day
-			int previousDay = -1;
-			int currentDay = 0;
-
 			// Enumerate the results
-			List<List<string>>.Enumerator enumerator = responseData.GetEnumerator();
-			while ( ( currentDay > previousDay ) && ( enumerator.MoveNext() == true ) )
+			int index = 0;
+			List<string>.Enumerator enumerator = dates.GetEnumerator();
+			while ( enumerator.MoveNext() == true )
 			{
-				previousDay = currentDay;
+				string date = enumerator.Current;
 
-				List<string> availRow = enumerator.Current;
-
-				// Get day of week, date and price
-				// Format is 'ddd-nn' so look for a string >= 6 chars and 4th char is '-'
-				foreach ( string value in availRow )
+				// Format is 'yyyy-mm-dd'
+				if ( date.Length == 10 )
 				{
-					if ( ( value.Length >= 6 ) && ( value[ 3 ] == '-' ) )
-					{
-						string date = value.Substring( 4, 2 );
-						currentDay = Int32.Parse( date );
+					DateTime fullDate = DateTime.Parse( date );
 
-						// Check for incrementing day number
-						if ( currentDay > previousDay )
-						{
-							// Add an entry for this date to the dictionary if not already there
-							availability.AddAvailability( 
-								string.Format( "{0}, {1} {2}, 2018", value.Substring( 0, 3 ), monthName, date ), 
-								( value.Length > 6 ) && ( value[ 6 ] == '&' ) );
-						}
+					// Add an entry for this date to the dictionary if not already there
+					if ( availabilities != null )
+					{
+						availability.AddAvailability( 
+							string.Format( "{0}, {1} {2}, 2018", fullDate.DayOfWeek.ToString().Substring( 0, 3 ), fullDate.ToString( "MMM" ), fullDate.Day ), 
+							( availabilities[ index ].Length > 6 ) && ( availabilities[ index ][ 0 ] == '&' ), hostelCount );
+					}
+					else
+					{
+						availability.AddAvailability(
+							string.Format( "{0}, {1} {2}, 2018", fullDate.DayOfWeek.ToString().Substring( 0, 3 ), fullDate.ToString( "MMM" ), fullDate.Day ), false, hostelCount );
 					}
 				}
+
+				index++;
 			}
 		}
 
@@ -163,9 +166,6 @@ namespace YHAAvailability
 					// Save the workbook
 					workbookPart.Workbook.Save();
 
-					// Now actually create somewhere to store the data
-					SheetData sheetData = worksheetPart.Worksheet.AppendChild( new SheetData() );
-
 					// Constructing header of hostel names
 					Row row = new Row();
 					row.Append( ConstructCell( " ", CellValues.String, 1 ) );
@@ -176,6 +176,9 @@ namespace YHAAvailability
 						row.Append( ConstructCell( hostel.Name, CellValues.String, 1 ) );
 						columns.Append( new Column { Min = columnNo, Max = columnNo++, Width = hostel.Name.Length + 1, CustomWidth = true } );
 					}
+
+					// Now actually create somewhere to store the data
+					SheetData sheetData = worksheetPart.Worksheet.AppendChild( new SheetData() );
 
 					// Insert the header row to the Sheet Data
 					sheetData.AppendChild( row );
@@ -332,7 +335,7 @@ namespace YHAAvailability
 			/// </summary>
 			/// <param name="date"></param>
 			/// <param name="available"></param>
-			public void AddAvailability( string date, bool available )
+			public void AddAvailability( string date, bool available, int hostelCount )
 			{
 				if ( dateFlags.ContainsKey( date ) == false )
 				{
@@ -340,7 +343,10 @@ namespace YHAAvailability
 					datesInOrder.Add( date );
 				}
 
-				dateFlags[ date ].Add( available );
+				if ( dateFlags[ date ].Count == hostelCount )
+				{
+					dateFlags[ date ].Add( available );
+				}
 			}
 
 			public List<bool> AvailabilityForDate( string dateString )
